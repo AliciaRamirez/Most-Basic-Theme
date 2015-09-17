182fad7f4dd939d5dcc44523b27b88406a2a455e
Alicia Ramirez <aramirez@rrc.ca>
2015-09-17 14:41:00 -0500

    Revert "First Class"
    This reverts commit bea38b3b694fb505853f215b8e4e80c713557e32.

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php get_header(); ?>
-    <div class="grid grid-pad">
-        <div class="col-8-12">Nothing here</div>
-        <div class="col-4-12">
-            <?php get_sidebar(); ?>
-        </div>
-    </div>
-<?php get_footer(); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Enter Title Here</title>
+
+    <?php wp_head(); ?>
+
+</head>
+<body>
+Nothing here
+
+<?php wp_footer(); ?>
+</body>
+</html>
+
+
+
+
